e1710155ba28b813cf3781ec56422d604d7a3a1d
trueapps <trueapps@trueapps-PC>
2018-05-30 12:55:05 +0200

    v PHP (singe a page) jsem doplnil kód pro základ šablony, vytvořil nový  PHP (content, vložil kód, zatím nejede, zprovozním

---
+++ wp-content/themes/zdenatvrda/content.php
@@ -0,0 +1,26 @@
+<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+  <?php 
+  if ( is_search()|| is_home() || is_archive()
+   || is_tag() || is_category() ) :
+  ?>
+ 
+  <h2 class="title">
+    <a href="<?php the_permalink() ?>" 
+      title="<?php the_title(); ?>">
+        <?php the_title(); ?>
+      </a>
+  </h2>
+ 
+      <div class="post-content">
+     <?php echo the_excerpt();?>
+   </div>
+ 
+  <?php else: ?>
+ 
+  <h1 class="entry-title"><?php the_title(); ?></h1>
+  <div class="post-content">
+<?php the_content(__('Read more...', 'theme')); ?>
+  </div>
+ 
+  <?php endif: ?>
+</div>
---
+++ wp-content/themes/zdenatvrda/single.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+<div class="row-fluid">
+  <div class="span8">
+ <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <!--BEGIN .hentry -->
+<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+  <h1 class="entry-title"><?php the_title(); ?></h1>
+<!--BEGIN .entry-content -->
+<div class="entry-content">
+<?php the_content(__('Read more...', 'theme')); ?>
+      </div>
+      <!--END .hentry-->  
+</div>
+<?php endwhile; else: ?>
+<div id="post-0" <?php post_class() ?>>
+<h1 class="entry-title">
+<?php _e('Error 404 - Not Found', 'theme') ?>
+                        </h1>
+<!--BEGIN .entry-content-->
+<div class="entry-content">
+<p><?php _e("Sorry, but you are looking for 
+something that isn't here.", "theme") ?></p>
+</div>
+</div>
+<?php endif; ?>
+</div>
+  <div class="span4">
+    <?php get_sidebar(); ?>
+  </div>
+</div>
+<?php get_footer(); ?>
