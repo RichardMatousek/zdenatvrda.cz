7435f13d79cd2b5d86b80343ac32dbb7355fd44d
trueapps <trueapps@trueapps-PC>
2018-06-05 15:16:17 +0200

    v CSS (style) změněna třída pro menu, přidán design jako u webu zdenatvrda.cz, v PHP (index) jsem vymazal část s články a postupně ho nahradím daty z webu zdenavrda.cz

--- wp-content/themes/zdenatvrda/index.php
+++ wp-content/themes/zdenatvrda/index.php
@@ -2,35 +2,9 @@
 <div style="height: 115px;"></div>
 <div class="col-xs-12 col-sm-6 col-md-8">
  
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	
-<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>             
-  <h2 class="title">
-  <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
-    <?php the_title(); ?>
-    </a>
-  </h2>
- 
-    <div class="post-content">
-  <?php echo the_excerpt();?>
-  </div>
-  </div>	
-  <?php 
-     endwhile; 
-     else: 
-  ?>
-<div id="post-0" class="post error404 not-found">
-<h1 class="entry-title"><?php _e( 'Not Found', 'theme' ); ?></h1>
-<div class="entry-content">
-<p><?php _e( 'Apologies, but no results were 
-                        found for the requested archive. Perhaps searching 
-                        will help find a related post.', 'theme' ); ?></p>
-</div><!-- .entry-content -->
-</div><!-- #post-0 -->
- 
-<?php endif; ?>
- 
+    <div>             
+  
+    </div>	 
 </div>
 
- 
 <?php get_footer(); ?>
